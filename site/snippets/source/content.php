0a932507325caba5c45a16c41f8972de294405de
Lukas Bestle <mail@lukasbestle.com>
2020-02-06 21:07:56 +0100

    Fix typos

--- site/snippets/source/content.php
+++ site/snippets/source/content.php
@@ -1,8 +1,8 @@
 <p>
   The content for this page is stored in a
   simple text file.
-<p>
+</p>
 <p>
   To learn more about Kirby's content folder and text format, please check out our docs:
   <a href="https://getkirby.com/docs">https://getkirby.com/docs</a>
-<p>
+</p>
