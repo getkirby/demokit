629ffee71dd8e5a623e3197a244b2a11f1bc0a74
Lukas Bestle <lukas@getkirby.com>
2021-03-03 21:02:09 +0100

    Remove temporary class overrides

--- index.php
+++ index.php
@@ -4,12 +4,6 @@ require __DIR__ . '/kirby/bootstrap.php';
 
 // load the code from the demo manager if available
 if (file_exists(__DIR__ . '/../../src/Demo/Demo.php') === true) {
-    // TODO: temporary overrides until the DB refactoring is in the core
-    require __DIR__ . '/../../vendor/getkirby/cms/src/Database/Database.php';
-    require __DIR__ . '/../../vendor/getkirby/cms/src/Database/Query.php';
-    require __DIR__ . '/../../vendor/getkirby/cms/src/Database/Sql.php';
-    require __DIR__ . '/../../vendor/getkirby/cms/src/Database/Sql/Sqlite.php';
-
     // autoload demo classes
     load([
         'Kirby\Demo\Config'    => __DIR__ . '/../../src/Demo/Config.php',
