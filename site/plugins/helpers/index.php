f30f068a561a380ff7bb306c72a89ad78a4b38ad
Lukas Bestle <lukas@getkirby.com>
2024-03-26 21:44:58 +0100

    Refactor helpers
    Ensures that the `@auto` styles are also queried by their path (not URL), which the KeyCDN plugin can process properly

--- site/plugins/helpers/index.php
+++ site/plugins/helpers/index.php
@@ -1,24 +1,25 @@
 <?php
 
+use Kirby\Cms\App;
+use Kirby\Filesystem\F;
+
 function blueprint($name) {
-	$file = kirby()->root('blueprints') . '/' . $name . '.yml';
+	$file = App::instance()->root('blueprints') . '/' . $name . '.yml';
 	return F::read($file);
 }
 
 function styles($example) {
-	$styles = [
+	$kirby    = App::instance();
+	$template = $kirby->site()->page()->template();
+	$styles   = [
 		'assets/lightbox/lightbox.css',
-		'assets/css/index.css'
+		'assets/css/index.css',
+		'assets/css/' . $example . '.css',
+		'assets/css/templates/' . $template . '.css'
 	];
 
-	$path = 'assets/css/' . $example . '.css';
-	$file = kirby()->root() . '/' . $path;
+	$root     = $kirby->root();
+	$filtered = array_filter($styles, fn($path) => file_exists($root . '/' . $path));
 
-	if (file_exists($file) === true) {
-		$styles[] = $path;
-	}
-
-	$styles[] = '@auto';
-
-	return css($styles);
+	return css($filtered);
 }
