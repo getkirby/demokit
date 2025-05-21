b3eada447cb112d49a51b0ba467c6722bb0dc4d5
Bastian Allgeier <mail@bastianallgeier.com>
2025-05-21 17:02:33 +0200

    feat: New social icons and style updates

--- site/snippets/blog/header.php
+++ site/snippets/blog/header.php
@@ -6,7 +6,10 @@
 		<span class="social">
 			<a href="<?= url('blog.rss') ?>"><?= svg('assets/icons/feed.svg') ?></a>
 			<a href="https://mastodon.social/@getkirby"><?= svg('assets/icons/mastodon.svg') ?></a>
-			<a href="https://instagram.com/getkirby"><?= svg('assets/icons/instagram.svg') ?></a>
+			<a href="https://chat.getkirby.com"><?= svg('assets/icons/discord.svg') ?></a>
+			<a href="https://bsky.app/profile/getkirby.com"><?= svg('assets/icons/bluesky.svg') ?></a>
+			<a href="https://www.linkedin.com/company/getkirby/"><?= svg('assets/icons/linkedin.svg') ?></a>
+			<a href="https://youtube.com/kirbycasts"><?= svg('assets/icons/youtube.svg') ?></a>
 		</span>
 	</nav>
 </header>
--- site/templates/home.php
+++ site/templates/home.php
@@ -7,14 +7,14 @@
 
 <section class="section">
 	<div class="grid">
-		<div class="column box" style="--columns: 6">
+		<div class="column" style="--columns: 6">
 			<div class="text margin-m">
 				<h2><?= $page->panelHeading()->escape() ?></h2>
 				<a href="<?= url('panel') ?>"><?= $page->image('panel.png')->asset()->html(['class' => 'rounded']) ?></a>
 			</div>
 			<a class="cta" href="<?= url('panel') ?>">Open the Panel</a>
 		</div>
-		<div class="column text transparent-box" style="--columns: 6">
+		<div class="column text" style="--columns: 6">
 			<h2><?= $page->aboutHeading()->escape() ?></h2>
 			<?= $page->aboutText()->permalinksToUrls() ?>
 		</div>
